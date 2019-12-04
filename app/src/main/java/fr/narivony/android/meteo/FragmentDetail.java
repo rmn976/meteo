522e73538782cf3c7e0b334822b1e802174f30b9
Nary Randria <rmn976@hotmail.fr>
2019-12-04 09:37:34 +0100

    Météo 34

---
+++ app/src/main/java/fr/narivony/android/meteo/FragmentDetail.java
@@ -0,0 +1,35 @@
+package fr.narivony.android.meteo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragmentDetail extends Fragment {
+    private Observation observation;
+
+    public FragmentDetail() {
+        // Required empty public constructor
+    }
+
+    void setObservation(Observation observation) {
+        this.observation = observation;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_detail, container, false);
+
+        return view;
+    }
+}
